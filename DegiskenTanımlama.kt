ad0df93b631c650269d570f0870fcb6c46013ae0
lordasiz07 <56899039+lordasiz07@users.noreply.github.com>
2022-06-06 00:26:40 +0300

    Create DegiskenTanımlama.kt
    Kotlin bootcamp ilk ödev

---
+++ DegiskenTanımlama.kt
@@ -0,0 +1,41 @@
+package com.mehmetkurt.techcareerhomework
+
+fun main() {
+    var islemNo=7
+    var ogrNo=1711404047
+    var kitapNo=42
+    var aTarih="05.06.2022"
+    var vTarih="12.2.2022"
+    var ogrAd="Mehmet Kurt"
+    var cinsiyet="Erkek"
+    var dTarih="12.22.2022"
+    var sınıf= "KOTLİN BOOTCAMP SINIFI"
+    var turNo=3
+    var sayfaSayisi=355
+    var puan=100
+    var yazarNo=55
+    var yazarAd="Mehmet"
+    var yazarSoyAd="Kurt"
+    var turAdi="Bilinmez"
+    println(islemNo)
+    println(ogrNo)
+    println(kitapNo)
+    println(aTarih)
+    println(vTarih)
+    println(ogrAd)
+    println(cinsiyet)
+    println(dTarih)
+    println(sınıf)
+    println(turNo)
+    println(sayfaSayisi)
+    println(puan)
+    println(yazarNo)
+    println(yazarAd)
+    println(yazarSoyAd)
+    println(turAdi)
+
+
+
+
+
+}
