dba55490395e560fef8d9f08cfc465856931837b
MEHMET KURT <56899039+mehmetkurt98@users.noreply.github.com>
2022-06-10 15:30:37 +0300

    KotlinBootcamp Ödev 2

---
+++ odevler/Odev2Main.kt
@@ -0,0 +1,20 @@
+package com.mehmetkurt.techcareerhomework.odevler
+
+fun main(){
+    val sonuc=Odev2()
+    println("Dereceni Fahreneit karşılığı: ${sonuc.soru1(100.0)}")
+    val sonuc2=Odev2()
+    println("Dikdörtgen çevre uzunluğu:${sonuc2.soru2(25,45)}")
+    val sonuc3=Odev2()
+    println("Bir sayının faktoriyel karşılığı: ${sonuc3.soru3(5)}")
+    val sonuc4=Odev2()
+    println("Kelimede en çok tekrar eden harf: ${sonuc4.soru4("maaa",'a')}")
+    val sonuc5=Odev2()
+    println("Kenar sayısına göre iç açılar: ${sonuc5.soru5(5)}")
+    val sonuc6=Odev2()
+    println("Çalışma saatine göre toplam ücret: ${sonuc6.soru6(50)}")
+    val sonuc7=Odev2()
+    println("Çalışma saatine göre toplam ücret: ${sonuc7.soru7(50)}")
+
+9
+}
