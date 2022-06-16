a32e4d55808e7467d463773df46caf73a3afcd55
lordasiz07 <56899039+lordasiz07@users.noreply.github.com>
2022-06-16 17:14:07 +0300

    tasarım çalışması ödevi

---
+++ Tasarım Ödevi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mehmetkurt.techcareerhomework.odevler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mehmetkurt.techcareerhomework.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
